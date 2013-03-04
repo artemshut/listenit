e8d73d384775e771261061388042bc7b121b187b
artemshut <artemshut@gmail.com>
2013-03-04 14:13:55 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listenit::Application.config.secret_token = '9622978e14de4d4694e1b29ea1cfbb80b887593182b06e8999842beaad798bc0cc9de82731c34bc3b17ba0a300279cf9b984c06341dd255cca0cf3e430b10185'
